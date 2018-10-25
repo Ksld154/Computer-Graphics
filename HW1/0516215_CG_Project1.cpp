0d058ef48a2c3685624d8652cd4da5854ff858d8
Alex <h427m4nn.cs05@g2.nctu.edu.tw>
2018-10-26 02:27:47 +0800

    Add files via upload

--- HW1/0516215_CG_Project1.cpp
+++ HW1/0516215_CG_Project1.cpp
@@ -12,12 +12,12 @@ using namespace std;
 int window_width  = 600;
 int window_height = 600;
 int degree = 20;
-float radius = 4.0;
-int earth_long = 360;
-int earth_lati = 180;
+float radius = 6.0;
+int earth_long = 360;    //slice
+int earth_lati = 180;    //stack
 
 int spin_degree = 1;
-float spin_deg = 0.5;
+float spin_deg = 0.1;    // X = spin_deg*365
 
 float degree_Earth_Revo = 0;
 float degree_Earth_Rotate = 0;
@@ -37,7 +37,6 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("0516215_Project1_SolarSystem");
 
-	Lighting();
 
 	glutDisplayFunc(display);
 	glutIdleFunc(Idle);
@@ -60,6 +59,11 @@ void display() {
 	//Viewport Matrix
 	glViewport(0, 0, window_width, window_height);
 
+	glClearDepth(1.0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	
 
 
 	// draw
@@ -67,28 +71,30 @@ void display() {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	float red[4]   = { 1.0f, 0.0f, 0.0f, 1.0f };
-	float green[4] = { 0.0f, 1.0f, 0.0f, 1.0f };
-	float blue[4]  = { 0.0f, 0.0f, 1.0f, 1.0f };
-	float white[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	Lighting();
+
+
+	float red[4]   = { 1.0f, 0.3f, 0.0f, 1.0f };
+	float blue[4]  = { 0.1f, 0.6f, 1.0f, 1.0f };
+	float moon[4]  = { 0.5f, 0.5f, 0.5f, 1.0f };
 
 
 	// draw earth
 	glPushMatrix();
 	
-	// ¤½Âà(revolution, i.e. rotate around the sun)
+	// å…¬è½‰(revolution, i.e. rotate around the sun)
 	glRotatef(degree_Earth_Revo, 0.0f, 1.0f, 0.0f);
 	glTranslatef(20.0, 0.0, 0.0);
-
-	// ¦ÛÂà(rotate itself)
+	// è‡ªè½‰(rotate itself)
 	glRotatef(-degree_Earth_Revo, 0.0f, 1.0f, 0.0f);
 	glRotatef(degree_Earth_Rotate, sin(23.5f * (2.0f*M_PI/360.0f)), cos(23.5f * (2.0f*M_PI/360.0f)), 0.0f);
 	glRotatef(-23.5, 0.0f, 0.0f, 1.0f);
 
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, blue);
-	glNormal3d(0, 10, 0);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, blue);
+	//glColor3f(0.0f, 0.0f, 1.0f);
+	
 	DrawSphere(radius/3, earth_long, earth_lati);
-	//glutWireCube(1);
 	glPopMatrix();
 	
 
@@ -96,17 +102,14 @@ void display() {
 	// draw earth rotation axis
 	glPushMatrix();
 
-	// ¤½Âà(revolution, i.e. rotate around the sun)
-	//glRotatef(-degree_Earth_Revo, 0.0f, 0.0f, 1.0f);
+	// å…¬è½‰(revolution, i.e. rotate around the sun)
 	glRotatef(degree_Earth_Revo, 0.0f, 1.0f, 0.0f);
 	glTranslatef(20.0, 0.0, 0.0);
-	
-	// ¦ÛÂà(rotate itself) 
+	// è‡ªè½‰(rotate itself) 
 	glRotatef(-degree_Earth_Revo, 0.0f, 1.0f, 0.0f);
 	glRotatef(-23.5, 0.0f, 0.0f, 1.0f);
 	
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, red);
-	glNormal3d(0, 10, 0);
+	glColor3f(1.0f, 0.0f, 0.0f);
 
 	glBegin(GL_LINES);
 	glColor3f (1.0f, 0.0f, 0.0f);
@@ -121,31 +124,29 @@ void display() {
 
 	// draw moon
 	glPushMatrix();
-
-	// (3) ¤½Âà(revolution, i.e. rotate around the SUN)
+	// (3) å…¬è½‰(revolution, i.e. rotate around the SUN)
 	glRotatef(degree_Earth_Revo, 0.0f, 1.0f, 0.0f);
 	glTranslatef(20.0, 0.0, 0.0);
-
-	// (2) ¤½Âà(revolution, i.e. rotate around the EARTH)
+	// (2) å…¬è½‰(revolution, i.e. rotate around the EARTH)
 	glRotatef(degree_Moon, 0.0f, 1.0f, 0.0f);
 	glTranslatef(3.0, 0.0, 0.0);
-
-	// (1) ¦ÛÂà(rotate itself)
+	// (1) è‡ªè½‰(rotate itself)
 	glRotatef(degree_Moon, 0.0f, 1.0f, 0.0f);
 
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, white);
-	glNormal3d(0, 10, 0);
-	DrawSphere(radius/6, 240, 60);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, moon);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, moon);
+	//glColor3f(0.5f, 0.5f, 0.5f);
 
+	DrawSphere(radius/6, 240, 60);
 	glPopMatrix();
 
 
-
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, red);
-	glNormal3d(0, 10, 0);
-
 	// draw sun
-	DrawSphere(radius, 240, 60);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, red);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, red);
+	//glColor3f(1.0f, 0.0f, 0.0f);
+	//Rotatef(degree_Moon, 0.0f, 1.0f, 0.0f);
+	DrawSphere(radius, 40, 20);
 
 	glutSwapBuffers();
 }
@@ -154,44 +155,72 @@ void display() {
 //4 2
 void DrawSphere(float r, int n_long, int n_lati) {
 	//F(u, v) = [cos(u)*sin(v)*r, cos(v)*r, sin(u)*sin(v)*r]
-	float x, y, z;
+	//float x, y, z;
+	
+	for (float i = 0; i < n_lati; i++) {
+
+		float SphereSurface[4][3] = { 0.0f, 0.0f, 0.0f };
+
+		for (float j = 0; j < n_long; j++) {
+
+			float m = i * (M_PI / n_lati);
+			float n = j * (2 * M_PI / n_long);
+
+
+
+			// x0, y0, z0
+			SphereSurface[0][0] = r * sin(m) * cos(n);
+			SphereSurface[0][1] = r * cos(m);
+			SphereSurface[0][2] = r * sin(m) * sin(n);
+
+			SphereSurface[1][0] = r * sin(m + (M_PI / n_lati)) * cos(n);
+			SphereSurface[1][1] = r * cos(m + (M_PI / n_lati));
+			SphereSurface[1][2] = r * sin(m + (M_PI / n_lati)) * sin(n);
+
+			SphereSurface[2][0] = r * sin(m) * cos(n + (2 * M_PI / n_long));
+			SphereSurface[2][1] = r * cos(m);
+			SphereSurface[2][2] = r * sin(m) * sin(n + (2 * M_PI / n_long));
+
+			SphereSurface[3][0] = r * sin(m + (M_PI / n_lati)) * cos(n + (2 * M_PI / n_long));
+			SphereSurface[3][1] = r * cos(m + (M_PI / n_lati));
+			SphereSurface[3][2] = r * sin(m + (M_PI / n_lati)) * sin(n + (2 * M_PI / n_long));
 	
 	
-	// draw latitudes  (horizontal)
-	for(float i = 0; i < M_PI; i += M_PI/n_lati) {
-		glBegin(GL_LINE_LOOP);
-		
-		y = (float)(r * cos(i));
-		for(float j = 0; j < 2*M_PI; j += 2*M_PI/n_long) {
-			x = (float)(r * sin(i) * cos(j));
-			z = (float)(r * sin(i) * sin(j));
-			glVertex3f(x, y, z);
+			
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			glBegin(GL_POLYGON);
+			
+			glVertex3f(SphereSurface[0][0], SphereSurface[0][1], SphereSurface[0][2]);
+			glVertex3f(SphereSurface[1][0], SphereSurface[1][1], SphereSurface[1][2]);
+			glVertex3f(SphereSurface[3][0], SphereSurface[3][1], SphereSurface[3][2]);
+			glVertex3f(SphereSurface[2][0], SphereSurface[2][1], SphereSurface[2][2]);
+
+			float v2[3] = { SphereSurface[1][0] - SphereSurface[0][0], SphereSurface[1][1] - SphereSurface[0][1], SphereSurface[1][2] - SphereSurface[0][2] };
+			float v1[3] = { SphereSurface[3][0] - SphereSurface[0][0], SphereSurface[3][1] - SphereSurface[0][1], SphereSurface[3][2] - SphereSurface[0][2] };
+
+			float normal[3]  = {v1[1]*v2[2]-v2[1]*v1[2], v2[0]*v1[2]-v1[0]*v2[2], v1[0]*v2[1]-v2[0]*v1[1]};
+			float normal_len = sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2]);
+			normal[0] = normal[0] / normal_len;
+			normal[1] = normal[1] / normal_len;
+			normal[2] = normal[2] / normal_len;
+			
+			glNormal3fv(normal);
+			
+			glEnd();
 		}
-		glEnd();
 	}
-	
-	
-	// draw longtitudes (vertical)
-	for(float i = 0; i < M_PI; i += 2*M_PI/n_long ){
-		glBegin(GL_LINE_LOOP);
-		for(float j = 0; j < 2*M_PI; j += M_PI/n_lati) {
-			x = (float)(r * sin(j) * cos(i));
-			y = (float)(r * cos(j));
-			z = (float)(r * sin(j) * sin(i));
-			glVertex3f(x, y, z);
-		}
-		glEnd();
-	}
-	
+
 }
 void Lighting() {
 	// enable lighting
+	//glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_LIGHTING);
 
 	//Add directed light
 	GLfloat diffuse_color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 	GLfloat ambient_color[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-	GLfloat position[] = { 0.0f, 10.0f, 0.0f, 1.0f };
+	GLfloat position[] = { 0.0f, 10.0f, 0.0f, 0.0f};
+	
 	glEnable(GL_LIGHT0);								//open light0
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_color);	//set diffuse color of light0
 	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_color);	//set ambient color of light0
@@ -200,10 +229,9 @@ void Lighting() {
 
 void Idle() {
 	Sleep(20);
-	//degree = (degree + spin_degree) % 360;
-	degree_Earth_Revo = DegreeCalc(degree_Earth_Revo, spin_deg);
+	degree_Earth_Revo   = DegreeCalc(degree_Earth_Revo, spin_deg);
 	degree_Earth_Rotate = DegreeCalc(degree_Earth_Rotate, spin_deg*365.0f);
-	degree_Moon = DegreeCalc(degree_Moon, spin_deg*28.0f);
+	degree_Moon = DegreeCalc(degree_Moon, spin_deg*(28.0f*360.0f/365.0f));
 	glutPostRedisplay();
 }
 
@@ -216,16 +244,6 @@ float DegreeCalc(float deg, float spin_angle){
 
 void keyboard(unsigned char key, int x, int y) {
 	
-	
-	/*switch (key) {
-		case 'o':
-			if()
-			earth_long = 4;
-			earth_lati = 2;
-			break;
-		default:
-			break;
-	}*/
 	if (key == 'o') {
 		if (earth_long == 360 && earth_lati == 180) {
 			earth_long = 4;
@@ -235,6 +253,9 @@ void keyboard(unsigned char key, int x, int y) {
 			earth_long = 360;
 			earth_lati = 180;
 		}
+	}
+	else if (key == 'p') {
+		// do pause
 	}
 
 
